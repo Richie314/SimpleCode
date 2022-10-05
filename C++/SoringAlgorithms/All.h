f123c28245ae7d2123dd0c33305b7f51888a7db1
Riccardo Ciucci <60298539+Richie314@users.noreply.github.com>
2022-10-05 22:29:47 +0200

    Created "All.h": one to include them all

--- C++/SoringAlgorithms/All.h
+++ C++/SoringAlgorithms/All.h
@@ -1,2 +1,11 @@
+namespace sorting
+{
 #include "BubbleSort.h"
+#include "InsertionSort.h"
+#include "OddEvenSort.h"
+#include "SelectionSort.h"
+#include "ShellSort.h"
+#include "ShakerSort.h"
+#include "MergeSort.h"
 #include "QuickSort.h"
+}
